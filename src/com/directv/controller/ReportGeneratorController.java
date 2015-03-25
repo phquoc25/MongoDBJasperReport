838a1f5798f519f6d9d7ff8fd77a5734f9c2ec56
phquoc25 <phquoc25@gmail.com>
2015-03-25 15:59:43 +0700

    Author    :QuocPH
    Message :Commit the startable project to new branch named report

--- src/com/directv/controller/ReportGeneratorController.java
+++ src/com/directv/controller/ReportGeneratorController.java
@@ -6,7 +6,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -14,9 +17,11 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.export.HtmlExporter;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleHtmlExporterOutput;
 import net.sf.jasperreports.export.SimpleHtmlReportConfiguration;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import net.sf.jasperreports.j2ee.servlets.ImageServlet;
 import net.sf.jasperreports.web.util.WebHtmlResourceHandler;
 
@@ -31,22 +36,40 @@ import com.directv.services.IReportService;
 import com.jaspersoft.mongodb.connection.MongoDbConnection;
 
 @Controller
-@RequestMapping
+@RequestMapping("/main")
 public class ReportGeneratorController {
 	
 	@Autowired
 	@Qualifier(value="PDFService")
 	private IReportService reportService;
 	
-	@RequestMapping(value="/userReport", method=RequestMethod.GET)
-	public String reportHtml(HttpServletRequest request){
+	
+	/**
+	 * Handles and retrieves the download page
+	 * 
+	 * @return the name of the JSP page
+	 */
+    @RequestMapping(method = RequestMethod.GET)
+    public String getDownloadPage() {
+    	//logger.debug("Received request to show download page");
+    
+    	// Do your work here. Whatever you like
+    	// i.e call a custom service to do your business
+    	// Prepare a model to be used by the JSP page
+    	
+    	// This will resolve to /WEB-INF/jsp/downloadpage.jsp
+    	return "downloadpage";
+	}
+    
+	@RequestMapping(value="/userHtmlReport", method=RequestMethod.GET)
+	public String reportHtml(HttpServletRequest request, HttpServletResponse response){
 		//String template = "D:\\TrainingCodes\\MongoDB\\MongoDBWsAndReport\\src\\Table_Report.jrxml";
 		//ClassLoader classLoader = getClass().getClassLoader();
 		//String template = classLoader.getResource("\\WEB-INF\\Table_Report.jrxml").getPath();
 		String rootPath = request.getSession().getServletContext().getRealPath("/");
 		String template =  rootPath + "WEB-INF\\Table_Report.jrxml";		
 		//.getSession().getServletContext().getRealPath("/");
-		String output = rootPath + "WEB-INF\\tempReport.jsp";
+		String output = rootPath + "WEB-INF\\pages\\tempReport.jsp";
 		String mongoURI = "mongodb://localhost:27017/qph";
 		MongoDbConnection connection = null;
 		ModelAndView model = null;
@@ -84,8 +107,43 @@ public class ReportGeneratorController {
 		} catch (JRException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} /*catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 		return "tempReport";
+	}
+	
+	@RequestMapping(value="/userPDFReport", method=RequestMethod.GET)
+	public void showPdfReport(HttpServletRequest request, HttpServletResponse response){
+		String rootPath = request.getSession().getServletContext().getRealPath("/");
+		String template =  rootPath + "WEB-INF\\Table_Report.jrxml";		
+		String mongoURI = "mongodb://localhost:27017/qph";
+		MongoDbConnection connection = null;
+		
+		try {
+			connection = new MongoDbConnection(mongoURI, null, null);
+
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			JasperReport jasperReport = JasperCompileManager.compileReport(template);
+
+			JasperPrint jasperPrint = JasperFillManager.fillReport(
+					jasperReport, parameters, connection);
+
+			JRPdfExporter pdfExporter = new JRPdfExporter();
+			SimpleExporterInput exporterInput = new SimpleExporterInput(jasperPrint);
+			pdfExporter.setExporterInput(exporterInput);
+			
+			SimpleOutputStreamExporterOutput exporterOutput;
+			exporterOutput = new SimpleOutputStreamExporterOutput(response.getOutputStream());
+			pdfExporter.setExporterOutput(exporterOutput);
+			pdfExporter.exportReport();
+			
+		} catch (JRException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@RequestMapping(value="/showReport", method=RequestMethod.GET)
