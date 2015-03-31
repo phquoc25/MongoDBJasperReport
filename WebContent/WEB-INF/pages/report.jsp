2c2da0807e470e2e454e11f7dc156dbfd5c29100
Quoc PHAN <phquoc25@gmail.com>
2015-03-31 23:01:25 +0700

    committing some changes

--- WebContent/WEB-INF/pages/report.jsp
+++ WebContent/WEB-INF/pages/report.jsp
@@ -8,7 +8,7 @@
 </head>
 <body>
 	<h1>Report Page</h1>
-	<a href="/MongoDBWsAndReport/report/showpiechart">Show pie chart report</a>
+	<a href="/MongoDBWsAndReport/report/showchart">Show pie chart report</a>
 	<br>
 	<a href="/MongoDBWsAndReport/report/showbarchart">Show bar chart report</a>
 	<br>
--- src/com/directv/controller/ReportGeneratorController.java
+++ src/com/directv/controller/ReportGeneratorController.java
@@ -79,7 +79,7 @@ public class ReportGeneratorController {
     		ModelAndView modelAndView, ModelMap model, HttpServletRequest request, HttpServletResponse response) 
 		 {
 		logger.debug("Received request to download report type " + type);
-    	//daoImpl.initCollection();
+    	//reportServiceImpl.initCollection();
 		JRDataSource dataSource = new JRBeanCollectionDataSource(reportServiceImpl.getCollection());
     	JRDataSource dataSource1 = new JRBeanCollectionDataSource(reportServiceImpl.getCollection());
 		model.put("datasource", new JREmptyDataSource());
@@ -110,7 +110,7 @@ public class ReportGeneratorController {
     
  
     
-	@RequestMapping(value="/showpiechart", method=RequestMethod.GET)
+	@RequestMapping(value="/showchart", method=RequestMethod.GET)
 	public ModelAndView reportHtml(HttpServletRequest request, HttpServletResponse response){
 		
 		logger.debug("Received request to show report");
@@ -121,155 +121,7 @@ public class ReportGeneratorController {
 		ModelAndView modelAndView = null;
 		String reportBody = reportServiceImpl.getReportBody(request);
 		modelAndView = reportServiceImpl.generateReportPage(reportBody);
-		modelAndView.addObject("pdfLink", "/MongoDBWsAndReport/report/downloadpiechart?type=pdf");
 		return modelAndView;
 	}
 	
-	@RequestMapping(value="/showbarchart", method=RequestMethod.GET)
-	public ModelAndView showBarChart(HttpServletRequest request, HttpServletResponse response){
-		
-		logger.debug("Received request to show report");
-		reportServiceImpl.initCollection();
-		String rootPath = request.getSession().getServletContext().getRealPath("/");
-		String template =  rootPath + "WEB-INF\\BarChart.jasper";		
-		
-		ModelAndView modelAndView = null;
-		String reportBody = getReportBody(request, rootPath, template);
-		modelAndView = generateReportPage(reportBody);
-		modelAndView.addObject("pdfLink", "/MongoDBWsAndReport/report/downloadchart/bar?type=pdf");
-		return modelAndView;
-	}
-	
-	@RequestMapping(value="/showlinechart", method=RequestMethod.GET)
-	public ModelAndView showLineChart(HttpServletRequest request, HttpServletResponse response){
-		
-		logger.debug("Received request to show report");
-		daoImpl.initCollection();
-		String rootPath = request.getSession().getServletContext().getRealPath("/");
-		String template =  rootPath + "WEB-INF\\LineChart.jasper";		
-		
-		ModelAndView modelAndView = null;
-		String reportBody = getReportBody(request, rootPath, template);
-		modelAndView = generateReportPage(reportBody);
-		modelAndView.addObject("pdfLink", "/MongoDBWsAndReport/report/downloadchart/line?type=pdf");
-		return modelAndView;
-	}
-	
-	public ModelAndView generateReportPage(String reportBody) {
-		ModelAndView modelAndView;
-		ModelMap model = new ModelMap();
-		model.put("reportBody", reportBody);
-		//model.put("pdfLink", "/MongoDBWsAndReport/main/downloadpiechart?type=pdf");
-		modelAndView = new ModelAndView("downloadpage");
-		modelAndView.addAllObjects(model);
-		return modelAndView;
-	}
-	
-	public String getReportBody(HttpServletRequest request, String rootPath,
-			String template) {
-		JRDataSource dataSource = new JRBeanCollectionDataSource(reportServiceImpl.getCollection());
-		JRDataSource dataSource1 = new JRBeanCollectionDataSource(reportServiceImpl.getCollection());
-		BufferedReader reader = null;
-		StringBuffer reportBody = null;
-		try {
-			Map<String, Object> parameters = new HashMap<String, Object>();
-			parameters.put("SubDataSource", dataSource);
-			parameters.put("SubDataSource1", dataSource1);
-			parameters.put("ChartTitle", "Voice Usage: 'Utterances per Day'");
-			parameters.put("dateFormater", new SimpleDateFormat("yyyy-MM-dd"));
-			//JasperReport jasperReport = JasperCompileManager.compileReport(template);
-			
-			JasperPrint jasperPrint = JasperFillManager.fillReport(template, parameters, new JREmptyDataSource());
-			
-			request.getSession().setAttribute(ImageServlet.DEFAULT_JASPER_PRINT_SESSION_ATTRIBUTE, jasperPrint);
-			HtmlExporter exporterHTML = new HtmlExporter();
-			SimpleExporterInput exporterInput = new SimpleExporterInput(jasperPrint);
-			exporterHTML.setExporterInput(exporterInput);
-			SimpleHtmlExporterOutput exporterOutput;
-			
-			//exporterOutput = new SimpleHtmlExporterOutput(response.getOutputStream());
-			String outputFile = rootPath + "WEB-INF\\pages\\MyReportOutput" + request.getSession().getId() + ".jsp";
-			exporterOutput = new SimpleHtmlExporterOutput(outputFile);
-			exporterOutput.setImageHandler(new WebHtmlResourceHandler("image?image={0}"));
-			exporterHTML.setExporterOutput(exporterOutput);
-			
-			SimpleHtmlReportConfiguration reportExportConfiguration = new SimpleHtmlReportConfiguration();
-			reportExportConfiguration.setWhitePageBackground(false);
-			reportExportConfiguration.setRemoveEmptySpaceBetweenRows(true);
-			exporterHTML.setConfiguration(reportExportConfiguration);
-			exporterHTML.exportReport();
-			
-			//Read the output file in order to obtain the report contain
-			reader = new BufferedReader(new FileReader(outputFile));
-			reportBody = new StringBuffer();
-			String currentLine;
-			
-			boolean isStarted = false;
-			boolean isTerminated = false;
-			while((currentLine = reader.readLine()) != null){
-				if(currentLine.startsWith("<body")){
-					isStarted = true;
-					continue;
-				}
-				
-				if(currentLine.startsWith("</body")){
-					isTerminated = true;
-				}
-				
-				if(isStarted && !isTerminated){
-					reportBody.append(currentLine);
-				}
-			}
-			
-		} catch (JRException e) {
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			if(reader != null){
-				try {
-					reader.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-		return reportBody.toString();
-	}
-	
-	@RequestMapping(value = "/downloadbarchart", method = RequestMethod.GET)
-	public ModelAndView showBarChartMultiReport(
-			@RequestParam("type") String type, ModelAndView modelAndView,
-			ModelMap model, HttpServletRequest request,
-			HttpServletResponse response) {
-		logger.debug("Received request to download multi report");
-		// daoImpl.initCollection();
-		JRDataSource dataSource = new JRBeanCollectionDataSource(daoImpl.getCollection());
-    	JRDataSource dataSource1 = new JRBeanCollectionDataSource(daoImpl.getCollection());
-		model.put("datasource", new JREmptyDataSource());
-		model.put("format", type);
-		model.put("requestObject", request);
-		model.put("ChartTitle", "Utt per day");
-		model.put("SubDataSource", dataSource);
-		model.put("SubDataSource1", dataSource1);
-		model.put("dateFormater", new SimpleDateFormat("yyyy-MM-dd"));
-		// model.put("SubDataSource1", dataSource1);
-
-		// multiReport is the View of our application
-		// This is declared inside the /WEB-INF/jasper-views.xml
-		modelAndView = new ModelAndView("barChartMultiReport", model);
-
-		// Return the View and the Model combined
-		return modelAndView;
-	}
-	
-	@RequestMapping(value="/showResult", method=RequestMethod.GET)
-	public String showResult(HttpServletRequest request, HttpServletResponse response){
-		return "MyReportOutput"; 
-	}
 }
